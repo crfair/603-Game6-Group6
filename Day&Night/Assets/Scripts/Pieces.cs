7a271ba4fcb6fd1208f5d7ebafb09444a48d27e2
Tom Choi <jw95@live.cn>
2022-04-14 17:46:53 -0400

    comments added

--- Day&Night/Assets/Scripts/Pieces.cs
+++ Day&Night/Assets/Scripts/Pieces.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+// Rotation types
 public enum RotationStatus {
     Normal,
     RotateLeft,
@@ -11,16 +12,15 @@ public enum RotationStatus {
     
 }
 
-public enum PiecesTypes {
-    T,
-    J,
-    L
-}
-
 
 static class Extension
 {
-    
+    /// <summary>
+    /// Rollover to the next option in an enum
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="option"></param>
+    /// <returns></returns>
     public static T Next<T>(this T option) where T : struct, Enum
     {
         int newValue = (int)(object)option + 1;
@@ -28,6 +28,12 @@ static class Extension
         return (T)(object)newValue;
     }
 
+    /// <summary>
+    /// Rollover to the previous option in an enum
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="option"></param>
+    /// <returns></returns>
     public static T Previous<T>(this T option) where T : struct, Enum
     {
         int newValue = (int)(object)option - 1;
@@ -35,6 +41,13 @@ static class Extension
         return (T)(object)newValue;
     }
 
+    /// <summary>
+    /// If the value is out of bounds, it will return the minimum or the maximum value instead.
+    /// </summary>
+    /// <param name="num"></param>
+    /// <param name="min">The minimum value</param>
+    /// <param name="max">The maximum value</param>
+    /// <returns>The value in the bound</returns>
     public static float Bounds(this float num, float min, float max)
     {
         float output = num;
@@ -49,6 +62,12 @@ static class Extension
         return output;
     }
 
+    /// <summary>
+    /// Check if the number is in the bound
+    /// </summary>
+    /// <param name="num"></param>
+    /// <param name="gridSize">The size of a grid</param>
+    /// <returns></returns>
     public static bool ifInGrid(this int num, int gridSize) {
         return num >= 0 && num < gridSize;
     }
@@ -57,19 +76,29 @@ static class Extension
 
 public class Pieces : MonoBehaviour
 {
+
     public RotationStatus rotationStatus = RotationStatus.Normal;
     [SerializeField]
-    int pieces;
-    public Vector2Int[] piecesBlocks;
+    int pieces; // The number of blocks in single piece
+    // The collision for the piece in 4 direction
+    // The size of this should be 4 * pieces, the order in this list is Normal, RotateLeft, UpsideDown, RotateRight
+    public Vector2Int[] piecesBlocks; 
 
-
+    // If the current piece is being moved by the player
     private bool startMovingPiece = false;
+
+    // The canvas attached
     public PuzzleCanvas puzzleCanvas;
+    // The current position of the piece
     private List<Vector2Int> placedPositions = new List<Vector2Int>();
     private bool allowRotation = true;
+
+    // The initial position in the canvas
     private Vector2 originalPosition;
 
-
+    /// <summary>
+    /// Deregister the piece from the grid map
+    /// </summary>
     private void clearMap() {
         foreach (Vector2Int position in placedPositions)
         {
@@ -78,26 +107,40 @@ public class Pieces : MonoBehaviour
         placedPositions.Clear();
     }
 
-
+    /// <summary>
+    /// Remove current piece from the grid
+    /// </summary>
     public void resetToDefaultPosition() {
         clearMap();
         transform.position = originalPosition;
     }
 
-
+    /// <summary>
+    /// Placing the piece
+    /// </summary>
     public void piecesClicked() {
+
+        // Only accept left click
         if (!Input.GetMouseButtonDown(0))
         {
             return;
         }
-            allowRotation = false;
+
+        // disable rotation during the placement process
+        // this is set to avoid multithreading problem
+        // e.g placing the object and rotating the piece at the same time
+        allowRotation = false;
+
+        // The player has clicked on a piece
         if (!Internals.startMovingPieces)
         {
             clearMap();
             startMovingPiece = true;
             Internals.startMovingPieces = true;
         }
+        // The player tries to set the piece
         else {
+            // Check if all the blocks in the piece do not overlap with other blocks in the grid
             bool allowPlacement = true;
             Vector2Int currentGridPosition = PuzzleCanvasHelper.getGridPosition(Input.mousePosition, Internals.gridDimension);
             for (int i = 0; i < pieces; i++) {
@@ -106,9 +149,11 @@ public class Pieces : MonoBehaviour
             }
             if (allowPlacement)
             {
+                // The grid is ok to set the piece
                 startMovingPiece = false;
                 Internals.startMovingPieces = false;
 
+                // Register the block to the grid
                 for (int i = 0; i < pieces; i++)
                 {
                     placedPositions.Add(getBlockPosition(rotationStatus, i) + currentGridPosition);
@@ -134,27 +179,28 @@ public class Pieces : MonoBehaviour
     {
 
         if (startMovingPiece) {
+            // get the grid coordinate using the mouse position
             Vector2Int gridPosition = PuzzleCanvasHelper.getGridPosition(Input.mousePosition, Internals.gridDimension);
             if (gridPosition.x != -1)
             {
+                // The mouse is in the grid, the piece should snap to the grid
                 transform.position = PuzzleCanvasHelper.getPositionFromGrid(gridPosition);
             }
             else
             {
+                // The mouse is not in the grid, the piece should follow the mouse
                 transform.position = Input.mousePosition;
             }
 
             if (Input.GetMouseButtonDown(1))
             {
-                
+                // Undo the placement
                 resetToDefaultPosition();
                 startMovingPiece = false;
                 Internals.startMovingPieces = false;
-                //Debug.Log(Input.mousePosition);
-                //Debug.Log(PuzzleCanvasHelper.getGridPosition(Input.mousePosition, Internals.gridDimension));
             }
 
-
+            // Rotation
             if (Input.GetKeyDown(KeyCode.Space) && allowRotation)
             {
                 transform.Rotate(Vector3.forward * -90);
@@ -163,9 +209,13 @@ public class Pieces : MonoBehaviour
         }
 
     }
-
+    /// <summary>
+    /// Get the index in the list
+    /// </summary>
+    /// <param name="rotationStatus"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
     Vector2Int getBlockPosition(RotationStatus rotationStatus, int index) {
-
         return piecesBlocks[(int)rotationStatus * pieces + index];
     }
 }
--- Day&Night/Assets/Scripts/PuzzleCanvas.cs
+++ Day&Night/Assets/Scripts/PuzzleCanvas.cs
@@ -18,6 +18,7 @@ public class PuzzleCanvas : MonoBehaviour
     private List<GameObject> fixedBlocks = new List<GameObject>();
     private List<GameObject> specialBlocks = new List<GameObject>();
 
+    // Clear the map
     void setEmptyMap(int row, int column) {
         for (int i = 0; i < row; i++)
             for (int j = 0; j < column; j++)
@@ -35,6 +36,7 @@ public class PuzzleCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Debug output for displaying the grid map
         if (Input.GetKeyDown(KeyCode.Escape)) {
             string output = "";
             for (int i = 0; i < 10; i++) {
@@ -54,32 +56,18 @@ public class PuzzleCanvas : MonoBehaviour
             Debug.Log(output);
         }
 
-        //if (Input.GetKeyDown(KeyCode.R)) {
-        //    if (!Internals.startMovingPieces) {
-        //        ResetAllPieces();
-        //    }
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.T))
-        //{
-        //    List<Vector2Int> exploredBlocks = new List<Vector2Int>();
-        //    //bool result= false;
-        //    bool result = SearchPath(specialBlockPositions[0], specialBlockPositions[specialBlockPositions.Length - 1], savedMap, Internals.gridDimension, exploredBlocks);
-
-        //    if (result)
-        //    {
-        //        Debug.Log("Found");
-        //    }
-        //    else {
-        //        Debug.Log("Not Found");
-        //    }
-        //}
     }
 
 
+    /// <summary>
+    /// Validate if there is a path from the first special point to the last one.
+    /// Note that the special points  in the list between the first one and the last one is not checked.
+    /// </summary>
     public void ValidatePath() {
+        // Create an empty list for saving the explored blocks. This is needed for the recursive method working.
         List<Vector2Int> exploredBlocks = new List<Vector2Int>();
-        //bool result = false;
+
+        // Check if there is a path
         bool result = SearchPath(specialBlockPositions[0], specialBlockPositions[specialBlockPositions.Length - 1], savedMap, Internals.gridDimension, exploredBlocks);
 
         if (result)
@@ -92,32 +80,59 @@ public class PuzzleCanvas : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Return a bool value to state if the user can place a single block in the map to avoid overlapping
+    /// </summary>
+    /// <param name="position">The block position in the grid coordinate</param>
+    /// <param name="gridDimension">The size of the grid</param>
+    /// <returns>if current position is free for placement</returns>
     public bool AllowPlacement(Vector2Int position,Vector2Int gridDimension) {
+
+        // Check if the position is out of bounds
         if (position.x < 0 || position.y < 0) {
             return false;
         }
         if (position.x >= gridDimension.x || position.y >= gridDimension.y) {
             return false;
         }
+
         return savedMap[position.x, position.y] == 0;
     }
 
+    /// <summary>
+    /// Place a single block to the map
+    /// You should use AllowPlacement() to check if the position is free
+    /// </summary>
+    /// <param name="position">>The block position in the grid coordinate</param>
     public void PlaceSingleBlock(Vector2Int position)
     {
         savedMap[position.x, position.y] = 1;
     }
+
+    /// <summary>
+    /// Remove a single block in the map
+    /// </summary>
+    /// <param name="position">>The block position in the grid coordinate</param>
     public void ResetSingleBlock(Vector2Int position) {
         savedMap[position.x, position.y] = 0;
     }
 
-    //public void ResetAllPieces() {
-    //    GameObject[] objects = GameObject.FindGameObjectsWithTag("Pieces");
-    //    foreach (GameObject obj in objects) {
-    //        Pieces singlePiece = obj.GetComponent<Pieces>();
-    //        singlePiece.resetToDefaultPosition();
-    //    }
-    //}
+    /// <summary>
+    /// Remove all the pieces from the map
+    /// </summary>
+    public void ResetAllPieces() {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Pieces");
+        foreach (GameObject obj in objects) {
+            Pieces singlePiece = obj.GetComponent<Pieces>();
+            singlePiece.resetToDefaultPosition();
+        }
+   }
 
+    /// <summary>
+    /// Remove all preset blocks from the map
+    /// The GameObject should have a component of the class Blocks
+    /// </summary>
+    /// <param name="storedData">The stored blocks</param>
     public void ResetAllBlocks(List<GameObject> storedData)
     {
         foreach (GameObject block in storedData)
@@ -128,23 +143,46 @@ public class PuzzleCanvas : MonoBehaviour
         storedData.Clear();
     }
 
-    public void ResetStartEndBlocks() {
 
-    }
-
+    /// <summary>
+    /// Generate and place preset blocks using the stored data
+    /// </summary>
+    /// <param name="positions"> Positions of the stored blocks</param>
+    /// <param name="prefab">The sample image object for placing on the canvas</param>
+    /// <param name="storedData">The data structure of block objects to store in</param>
     public void GenerateBlocks(Vector2Int[] positions,GameObject prefab,List<GameObject> storedData) {
+
+        // Iterate each object in the list
         foreach (Vector2Int position in positions) {
+            // Get the position in the canvas coordinate
             Vector2 canvasPosition = PuzzleCanvasHelper.getPositionFromGrid(position);
+            // Generate a single block
             GameObject singleBlock = Instantiate(prefab);
+            // IMPORTANT: must set the parent first, attach the object to the canvas and then change other properties
             singleBlock.transform.SetParent(transform);
+            // Change the position and the scale
             singleBlock.transform.position = canvasPosition;
             singleBlock.transform.localScale = new Vector3(1, 1, 1);
             singleBlock.GetComponent<Blocks>().gridPosition = position;
+
+            // Register the block to the map system
             PlaceSingleBlock(position);
+
+            // Add to the data list for further usage
             storedData.Add(singleBlock);
         }
     }
 
+    /// <summary>
+    /// To check if the current position has a block
+    /// It will also check if the current block is not explored before
+    /// </summary>
+    /// <param name="map">The map data structure</param>
+    /// <param name="x">The x position in the grid coordinate</param>
+    /// <param name="y">The y position in the grid coordinate</param>
+    /// <param name="gridDimension">The size of the grid</param>
+    /// <param name="data">The list for return the valid data</param>
+    /// <param name="exploredBlocks">The list for explored blocks</param>
     private void AddNeighbor(int[,] map, int x, int y, Vector2Int gridDimension,List<Vector2Int> data, List<Vector2Int> exploredBlocks) {
         if (x.ifInGrid(gridDimension.x) && y.ifInGrid(gridDimension.y) && !exploredBlocks.Exists(block => block.x == x && block.y == y))
         {
@@ -152,12 +190,25 @@ public class PuzzleCanvas : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Search the rectangular neighbors in the current position
+    /// </summary>
+    /// <param name="map">The map data structure</param>
+    /// <param name="position">The position for searching the neighbors</param>
+    /// <param name="gridDimension">The grid size</param>
+    /// <param name="exploredBlocks">The list for storing explored blocks</param>
+    /// <returns>The vaild, undiscoverd positions of the neighbors for the current position</returns>
     private List<Vector2Int> SearchRectNeighbors(int[,] map, Vector2Int position, Vector2Int gridDimension, List<Vector2Int> exploredBlocks) {
+        // Create a list for storing output
         List<Vector2Int> output = new List<Vector2Int>();
+
+        // Check if the position itself is valid in the grid
         if (!position.x.ifInGrid(gridDimension.x) || !position.y.ifInGrid(gridDimension.y))
         {
             return output;
         }
+
+        // Add 4 rectangular neighbors
         AddNeighbor(map, position.x - 1, position.y, gridDimension, output, exploredBlocks);
         AddNeighbor(map, position.x + 1, position.y, gridDimension, output, exploredBlocks);
         AddNeighbor(map, position.x, position.y + 1, gridDimension, output, exploredBlocks);
@@ -165,23 +216,40 @@ public class PuzzleCanvas : MonoBehaviour
         return output;
     }
 
+    /// <summary>
+    /// Search if there is a path from point A to point B in the grid system
+    /// </summary>
+    /// <param name="start">The position of the starting point</param>
+    /// <param name="end">The position of the destination</param>
+    /// <param name="map">The map data structure</param>
+    /// <param name="gridDimension">The grid size</param>
+    /// <param name="exploredBlocks">The list for storing explored blocks, this should be an initialized empty list if called from outside</param>
+    /// <returns>if there is a path</returns>
     public bool SearchPath(Vector2Int start, Vector2Int end, int[,] map, Vector2Int gridDimension,List<Vector2Int> exploredBlocks) {
 
+        // Add current positions to the explored ones
         exploredBlocks.Add(start);
+
+        //Adding objects to show the search path
         //Vector2 canvasPosition = PuzzleCanvasHelper.getPositionFromGrid(start);
         //GameObject singleBlock = Instantiate(testObject);
-        //singleBlock.transform.position = canvasPosition;
         //singleBlock.transform.SetParent(transform);
+        //singleBlock.transform.position = canvasPosition;
         //singleBlock.transform.localScale = new Vector3(1, 1, 1);
 
+        // Search the rectangular neighbors
         List<Vector2Int> neighbors = SearchRectNeighbors(map, start, gridDimension, exploredBlocks);
+
+        // Devide the problem into four sub problem
         bool result = false;
         foreach (Vector2Int position in neighbors) {
             if (position == end)
             {
+                // if the vaild neignbor is the destination then we find the path
                 return true;
             }
             else{
+                // IfPath(currentPosition,destination) = IfPath(neighborLeft,destination) || IfPath(NeighborRight,destination) || IfPath(NeighborUp,destination) || IfPath(NeighborDown,destination)
                 result = result || SearchPath(position, end, map, gridDimension, exploredBlocks);
             }
         }
